42cdba864ec7b9e07be1e77b11730ce2de31901c
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-22 18:48:52 -0300

    Atualiza versão para fluxo promocional e câmeras frontal e traseira

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.autombot.security"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
