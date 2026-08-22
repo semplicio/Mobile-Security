ca64e61b7debfcc65728fc925069abaf0b33cc3f
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-22 13:21:50 -0300

    Atualiza versão após correção do Gmail OAuth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.autombot.security"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
