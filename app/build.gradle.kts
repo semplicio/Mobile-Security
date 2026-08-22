620294ca6a44c08b3149a29cede6d2da5ed38c49
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-22 19:47:41 -0300

    Atualiza versão para nova identidade visual do aplicativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.autombot.security"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
