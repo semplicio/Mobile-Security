e96fed51138a0a22b0e9920830a6d0c7028de7a1
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-22 22:24:40 -0300

    build: bump Mobile Security to 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.autombot.security"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
