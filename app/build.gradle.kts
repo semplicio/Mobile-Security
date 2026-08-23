3bd142b045c3a8055550dfd4c6daa2a07e1fd23c
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-22 21:59:41 -0300

    chore: bump app version to 0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.autombot.security"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
