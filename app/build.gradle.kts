a679dfdd5a52612c4ef5b2080a77b76d9ed4e0d2
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-24 21:18:29 -0300

    Atualiza versão para 0.1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.autombot.security"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 11
+        versionName = "0.1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
